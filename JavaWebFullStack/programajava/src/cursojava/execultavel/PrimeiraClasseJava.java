586296a38d82d62f024fd2cdca4537900e52139b
Givaldo Medeiros Neto <givaldo@gmsn.dev.br>
2025-09-03 10:46:20 -0300

    Inserindo Projetos

---
+++ JavaWebFullStack/programajava/src/cursojava/execultavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.execultavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Meu Primeiro Programa Java, Hello Word!");
+		
+	}
+
+}
