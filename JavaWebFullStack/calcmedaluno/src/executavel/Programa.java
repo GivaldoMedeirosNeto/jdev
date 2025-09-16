3bb30dc8af0f7afaf941bdc61d1dba46092462b0
Givaldo <givaldo.ctinf@outlook.com>
2025-09-16 10:03:57 -0300

    Aula de Lista.

--- JavaWebFullStack/calcmedaluno/src/executavel/Programa.java
+++ JavaWebFullStack/calcmedaluno/src/executavel/Programa.java
@@ -3,6 +3,7 @@ package executavel;
 import javax.swing.JOptionPane;
 
 import model.Aluno;
+import model.Disciplina;
 
 public class Programa {
 	
@@ -11,19 +12,28 @@ public class Programa {
 		
 		Aluno aluno = new Aluno();
 		
+		
 		aluno.setNome(JOptionPane.showInputDialog("Nome do Aluno: "));
 		aluno.setnEscola(JOptionPane.showInputDialog("Nome da Escola: "));
-			
 		
-		System.out.println("Aluno: " + aluno.getNome());
-		System.out.println("Escola: " + aluno.getnEscola());
+		for(int pos = 1; pos <= 4; pos++) {
+			
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(JOptionPane.showInputDialog("Qual Disciplina " + pos + " de 4: "));
+			disciplina.setNota(
+				Double.valueOf(
+					JOptionPane.showInputDialog("Qual nota para disciplina " + disciplina.getDisciplina() + ":")
+				)
+			);
+			aluno.getDisciplinas().add(disciplina);
+			
+		}
+		
+		System.out.println("Aluno: " + aluno);
 		System.out.println("Média do aluno: " + aluno.getMedia());
-		System.out.println(aluno.getAprovadoString());
 		System.out.println(aluno.getAprovado() ? "Aprovado" : "Reprovado");
 		
-		System.out.println("***************************************");
-		
-		System.out.println(aluno);		
+		System.out.println("***************************************");		
 		
 	}
 
