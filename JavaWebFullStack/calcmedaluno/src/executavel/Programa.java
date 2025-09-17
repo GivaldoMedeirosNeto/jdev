f0d8ad53c3a2c9ee0cf110ad011218aa347eeb15
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-09-17 16:36:20 -0300

    Deixando a removão das disciplinas mais dinamicas.

--- JavaWebFullStack/calcmedaluno/src/executavel/Programa.java
+++ JavaWebFullStack/calcmedaluno/src/executavel/Programa.java
@@ -12,7 +12,7 @@ public class Programa {
 		
 		Aluno aluno = new Aluno();
 		int qtdDisc = 0;
-		
+		int remover;
 		
 		aluno.setNome(JOptionPane.showInputDialog("Nome do Aluno: "));
 		aluno.setnEscola(JOptionPane.showInputDialog("Nome da Escola: "));
@@ -23,7 +23,7 @@ public class Programa {
 			Disciplina disciplina = new Disciplina();
 			
 			disciplina.setDisciplina(
-				JOptionPane.showInputDialog("Qual Disciplina " + pos + " de " + qtdDisc +": ")
+				JOptionPane.showInputDialog("Qual o nome da Disciplina " + pos + " de " + qtdDisc +": ")
 			);
 			
 			disciplina.setNota(
@@ -36,12 +36,26 @@ public class Programa {
 			
 		}
 		
-		int remover = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina");
+		do {
+			
+			StringBuilder listaFormatada = new StringBuilder();
+			for (int i = 0; i < aluno.getDisciplinas().size(); i++) {
+			    listaFormatada.append(i + " - " + aluno.getDisciplinas().get(i) + "\n");
+			}
+			
+			remover = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+			if(remover == 0) {
+				
+				String discRemovida = JOptionPane.showInputDialog("Qual Disciplina?\n" + listaFormatada);
+				if(discRemovida != null) {
+					int index = Integer.valueOf(discRemovida);
+					aluno.getDisciplinas().remove(index);
+				}
+				
+			}
+			
+		} while ( remover ==0 );
 		
-		if(remover == 0) {
-			int discRemovida = Integer.valueOf(JOptionPane.showInputDialog("Qual disciplina?"));
-			aluno.getDisciplinas().remove(discRemovida-1);
-		}
 		
 		System.out.println("Aluno: " + aluno);
 		System.out.println("Média do aluno: " + aluno.getMedia());
@@ -49,7 +63,7 @@ public class Programa {
 		System.out.println(aluno.getAprovadoString());
 		
 		System.out.println("***************************************");		
-		
+
 	}
 
 }
