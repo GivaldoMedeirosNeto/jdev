9ce54e5716051e8c52158b24bb42baaea29cb85c
Givaldo <givaldo.ctinf@outlook.com>
2025-09-23 15:42:19 -0300

    Lista de Alunos

--- JavaWebFullStack/calcmedaluno/src/executavel/Programa.java
+++ JavaWebFullStack/calcmedaluno/src/executavel/Programa.java
@@ -1,5 +1,8 @@
 package executavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 import model.Aluno;
@@ -10,59 +13,93 @@ public class Programa {
 	/* Programa Calc Média do Aluno */
 	public static void main(String[] args) {
 		
-		Aluno aluno = new Aluno();
+		List<Aluno> LisAlunos = new ArrayList<Aluno>();
 		int qtdDisc = 0;
+		int qtdAluno = 1;
 		int remover;
-		
-		aluno.setNome(JOptionPane.showInputDialog("Nome do Aluno: "));
-		aluno.setnEscola(JOptionPane.showInputDialog("Nome da Escola: "));
-		qtdDisc = Integer.valueOf(JOptionPane.showInputDialog("Quantas Disciplinas: "));
-		
-		for(int pos = 1; pos <= qtdDisc; pos++) {
-			
-			Disciplina disciplina = new Disciplina();
-			
-			disciplina.setDisciplina(
-				JOptionPane.showInputDialog("Qual o nome da Disciplina " + pos + " de " + qtdDisc +": ")
-			);
-			
-			disciplina.setNota(
-				Double.valueOf(
-					JOptionPane.showInputDialog("Qual nota para disciplina " + disciplina.getDisciplina() + ":")
-				)
-			);
-			
-			aluno.getDisciplinas().add(disciplina);
-			
-		}
 		
 		do {
 			
-			StringBuilder listaFormatada = new StringBuilder();
-			for (int i = 0; i < aluno.getDisciplinas().size(); i++) {
-			    listaFormatada.append(i + " - " + aluno.getDisciplinas().get(i) + "\n");
+			String entrada = JOptionPane.showInputDialog("Quantos alunos deseja inserir? ");
+			
+			if(entrada != null && entrada.matches("\\d+")) {
+				qtdAluno = Integer.parseInt(entrada);
 			}
 			
-			remover = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-			if(remover == 0) {
+			if(entrada == null) {
+				JOptionPane.showMessageDialog(null, "Será adicionando somente 1 aluno.");
+			} else {
+				JOptionPane.showMessageDialog(null, "Será adicionando " + qtdAluno + " aluno(s).");
+			}
+			
+		} while(qtdAluno < 1);
+		
+		
+		for(int qtd = 1; qtd <= qtdAluno; qtd++) {
+			
+			Aluno aluno = new Aluno();
+			
+			aluno.setNome(JOptionPane.showInputDialog("Qual o nome do Aluno: "));
+			aluno.setnEscola(JOptionPane.showInputDialog("Qual o nome da Escola: "));
+			qtdDisc = Integer.valueOf(
+				JOptionPane.showInputDialog("Quantas Disciplinas para o aluno " + aluno.getNome() +":")
+			);
+			
+			for(int pos = 1; pos <= qtdDisc; pos++) {
 				
-				String discRemovida = JOptionPane.showInputDialog("Qual Disciplina?\n" + listaFormatada);
-				if(discRemovida != null) {
-					int index = Integer.valueOf(discRemovida);
-					aluno.getDisciplinas().remove(index);
+				Disciplina disciplina = new Disciplina();
+				
+				disciplina.setDisciplina(
+					JOptionPane.showInputDialog("Qual o nome da Disciplina " + pos + " de " + qtdDisc +": ")
+				);
+				
+				disciplina.setNota(
+					Double.valueOf(
+						JOptionPane.showInputDialog("Qual nota para disciplina " + disciplina.getDisciplina() + ":")
+					)
+				);
+				
+				aluno.getDisciplinas().add(disciplina);
+				
+			}
+			
+			do {
+				
+				StringBuilder listaFormatada = new StringBuilder();
+				for (int i = 0; i < aluno.getDisciplinas().size(); i++) {
+				    listaFormatada.append(i + " - " + aluno.getDisciplinas().get(i) + "\n");
 				}
 				
-			}
+				remover = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+				if(remover == 0) {
+					
+					String discRemovida = JOptionPane.showInputDialog("Qual Disciplina?\n" + listaFormatada);
+					if(discRemovida != null) {
+						int index = Integer.valueOf(discRemovida);
+						aluno.getDisciplinas().remove(index);
+					}
+					
+				}
+				
+			} while ( remover == 0 );
 			
-		} while ( remover ==0 );
+			LisAlunos.add(aluno);
+			
+		}
 		
+		System.out.println("***************************************");
 		
-		System.out.println("Aluno: " + aluno);
-		System.out.println("Média do aluno: " + aluno.getMedia());
-		// System.out.println(aluno.getAprovado() ? "Aprovado" : "Reprovado");
-		System.out.println(aluno.getAprovadoString());
+		for (Aluno aluno : LisAlunos) {
+			System.out.println("Aluno: " + aluno);
+			System.out.println("Média do aluno: " + aluno.getMedia());
+			System.out.println(aluno.getAprovadoString());
+		}
 		
-		System.out.println("***************************************");		
+		System.out.println("***************************************");
+		
+		/* Operador Ternário
+		System.out.println(aluno.getAprovado() ? "Aprovado" : "Reprovado");
+		*/	
 
 	}
 
