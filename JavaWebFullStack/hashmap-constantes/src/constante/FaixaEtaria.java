831d78c07c3757a8719499bed3289e62016bb0ff
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-09-27 19:13:35 -0300

    Aula de Constantes

---
+++ JavaWebFullStack/hashmap-constantes/src/constante/FaixaEtaria.java
@@ -0,0 +1,10 @@
+package constante;
+
+public class FaixaEtaria {
+	
+	public static String CRIANCA = "Criança";
+	public static String ADOLESCENTE = "Adolescente";
+	public static String ADULTO = "Adulto";
+	public static String VIDIDO = "Vivido";
+
+}
