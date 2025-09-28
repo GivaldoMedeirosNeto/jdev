53b9addd84572b49b60c5f390335edf5b2fdb18f
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-09-27 21:49:22 -0300

    Classe Disciplina

---
+++ JavaWebFullStack/proj-escola/src/model/Disciplina.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.Objects;
+
+public class Disciplina {
+	
+	private double nota;
+	private String disciplina;
+	
+	/* Getters */
+	public double getNota() {
+		return nota;
+	}
+	
+	public String getDisciplina() {
+		return disciplina;
+	}
+	
+	/* Setters */
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+		
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+
+	/* Equals e Hashcode */
+	@Override
+	public int hashCode() {
+		return Objects.hash(disciplina, nota);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disciplina other = (Disciplina) obj;
+		return Objects.equals(disciplina, other.disciplina)
+				&& Double.doubleToLongBits(nota) == Double.doubleToLongBits(other.nota);
+	}
+
+	/* ToString */
+	@Override
+	public String toString() {
+		return "[disciplina = " + disciplina + ", nota = " + nota + "]";
+	}
+
+}
