355452d63c957d0cdd7331930444c2ac31ee9e36
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-09-27 21:37:09 -0300

    Constantes

---
+++ JavaWebFullStack/proj-escola/src/constante/StatusAluno.java
@@ -0,0 +1,9 @@
+package constante;
+
+public class StatusAluno {
+	
+	public static String APROVADO = "Aprovado";
+	public static String REPROVADO = "Reprovado";
+	public static String RECUPECACAO = "Recuperação";
+
+}
