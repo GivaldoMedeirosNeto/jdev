29d1439f8fa60aae2dfc0a8207960912cbfe7dd7
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-09-27 21:18:24 -0300

    Correção para aula.

--- JavaWebFullStack/calcmedaluno/src/model/StatusAluno.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class StatusAluno {
-	
-	public static String APROVADO = "Aprovado";
-	public static String REPROVADO = "Reprovado";
-	public static String RECUPECACAO = "Recuperação";
-
-}
