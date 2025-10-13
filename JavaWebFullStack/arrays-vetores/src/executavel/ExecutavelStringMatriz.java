d7f8ed616a0f440b90b4383ecd92a71ed4859bd7
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-10-13 18:35:07 -0300

    Conversão de matriz em array

--- JavaWebFullStack/arrays-vetores/src/executavel/ExecutavelStringMatriz.java
+++ JavaWebFullStack/arrays-vetores/src/executavel/ExecutavelStringMatriz.java
@@ -1,5 +1,8 @@
 package executavel;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ExecutavelStringMatriz {
 	
 	public static void main(String[] args) {
@@ -10,6 +13,24 @@ public class ExecutavelStringMatriz {
 		
 		for(int i = 0; i < valores.length; i++) {
 			System.out.println(valores[i].replaceAll(" ", ""));
+		}
+		
+		System.out.println();		
+		
+		/* Convertendo um Array em Lista */
+		List<String> list = Arrays.asList(valores);
+		
+		for (String valor : list) {
+			System.out.println(valor);
+		}
+		
+		System.out.println();
+		
+		/* Convertendo uma Lista em Array */
+		String[] conversao = list.toArray(new String[list.size()]);
+		
+		for(int i = 0; i < conversao.length; i++) {
+			System.out.println(conversao[i]);
 		}
 		
 	}
