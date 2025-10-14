465f4dadb8f64f23f6b8923104d1cd72613980c7
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-10-14 07:18:30 -0300

    Melhorando o cod.

--- JavaWebFullStack/thread-aula/src/executavel/AulaThread.java
+++ JavaWebFullStack/thread-aula/src/executavel/AulaThread.java
@@ -30,34 +30,41 @@ public class AulaThread {
 			}			
 		}.start();
 		
-		new Thread() {			
-			public void run() {
-				for(int i = 1; i <= 10; i++) {					
-					try {
-						Thread.sleep(2000);
-						if(i == 1) {
-							System.out.println("Enviando " + retorno(i) + " email");
-						} else {
-							System.out.println("Enviando " + retorno(i) + " emails");
-						}
-						
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}					
-				}
-			}			
-		}.start();
+		Thread email = new Thread(thread1);
+		email.start();
 		
 		JOptionPane.showMessageDialog(null, "Executando em segundo plano o envio.");
 		
 	}
 	
 	public static int retorno(int num) {
-		int soma = 1;
+		int soma = 0;
 		for(int x = 1; x <= num; x++) {
-			soma *= num;
+			soma += num;
 		}		
 		return soma;
 	}
+	
+	private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			
+			for(int i = 1; i <= 10; i++) {					
+				try {
+					Thread.sleep(2000);
+					if(i == 1) {
+						System.out.println("Enviando " + retorno(i) + " email");
+					} else {
+						System.out.println("Enviando " + retorno(i) + " emails");
+					}
+					
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}					
+			}			
+			
+		}
+	};
 
 }
