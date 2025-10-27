bc8a98f5857fd0554b49751dedb2a1e390c1515a
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-10-27 18:57:43 -0300

    Editando no apache poi

---
+++ JavaWebFullStack/txt-excel/src/apache/EditarApachePoi.java
@@ -0,0 +1,44 @@
+package apache;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class EditarApachePoi {
+	
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("E:\\eclipse-workspace\\cursos\\Jdev\\JavaWebFullStack\\txt-excel\\arquivo.xls");
+		FileInputStream entrada = new FileInputStream(file);
+		HSSFWorkbook arquivo = new HSSFWorkbook(entrada);
+		HSSFSheet planilha = arquivo.getSheetAt(0);
+		
+		Iterator<Row> linhaIterator = planilha.iterator();
+		
+		while(linhaIterator.hasNext()) {
+			Row linha = linhaIterator.next();
+			
+			int nCell = linha.getPhysicalNumberOfCells();
+			Cell cell = linha.createCell(nCell);
+			cell.setCellValue("teste");
+		}
+		
+		entrada.close();
+		FileOutputStream saida = new FileOutputStream(file);
+		arquivo.write(saida);
+		saida.flush();
+		saida.close();
+		arquivo.close();
+		
+		System.out.println("planilha atualizada.");
+		
+	}
+
+}
