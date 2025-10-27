5900973c3d8803d400bcedba1110a05dcfbec2fe
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-10-27 15:27:42 -0300

    Nova API java

--- JavaWebFullStack/datas-horas/src/executavel/NovaApiData.java
+++ JavaWebFullStack/datas-horas/src/executavel/NovaApiData.java
@@ -1,14 +1,17 @@
 package executavel;
 
 import java.text.ParseException;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class NovaApiData {
 	
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, InterruptedException {
 		
 		LocalDate dtAtual = LocalDate.now();		
 		System.out.println("Data: " + dtAtual);
@@ -25,6 +28,45 @@ public class NovaApiData {
 		System.out.println("Dia: " + dataHora.getDayOfMonth());
 		System.out.println("Mês: " + dataHora.getMonth());
 		System.out.println("Ano: " + dataHora.getYear());
+		
+		System.out.println("**************************************");
+		
+		Instant inicio = Instant.now();
+		Thread.sleep(5000);
+		Instant fim = Instant.now();
+		Duration duracao = Duration.between(inicio, fim);
+		
+		System.out.println(duracao.toSeconds());
+		
+		System.out.println("**************************************");
+		
+		LocalDate datAtinga = LocalDate.parse("2020-04-05");
+		LocalDate datNova = LocalDate.parse("2021-05-08");
+		LocalDate datAtual = LocalDate.parse("2021-04-05");
+		
+		System.out.println(datAtinga.isAfter(datNova));
+		System.out.println(datAtinga.isBefore(datNova));
+		System.out.println(datAtual.isEqual(datNova));
+		
+		Period periodo = Period.between(datAtinga, datNova);
+		System.out.println(periodo.getYears() + " ano, " + periodo.getMonths() + " meses, " + periodo.getDays() + " dias. ");
+		System.out.println(periodo.toTotalMonths());
+		
+		System.out.println("**************************************");
+		
+		LocalDate dataBase = LocalDate.parse("2021-10-10");
+		System.out.println(dataBase);
+		System.out.println(dataBase.plusDays(5));
+		System.out.println(dataBase.plusWeeks(5));
+		System.out.println(dataBase.plusMonths(5));
+		System.out.println(dataBase.plusYears(5));
+		
+		System.out.println("**************************************");
+		
+		for(int i = 1; i <= 12; i++ ) {
+			dataBase = dataBase.plusMonths(1);
+			System.out.println(dataBase);
+		}
 		
 	}
 
