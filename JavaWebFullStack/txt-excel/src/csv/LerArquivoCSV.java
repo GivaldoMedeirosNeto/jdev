8172ba0f7297e2ffdbbe7de219372674b126b4ac
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2025-10-27 17:37:15 -0300

    lendo arquivo csv

---
+++ JavaWebFullStack/txt-excel/src/csv/LerArquivoCSV.java
@@ -0,0 +1,40 @@
+package csv;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import arquivos.Pessoa;
+
+public class LerArquivoCSV {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		FileInputStream arquivo = new FileInputStream(
+				new File("E:\\eclipse-workspace\\cursos\\Jdev\\JavaWebFullStack\\txt-excel\\arquivo.csv"));
+		
+		Scanner lerArquivo = new Scanner(arquivo, "UTF-8");
+		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		while(lerArquivo.hasNext()) {
+			String linha = lerArquivo.nextLine();
+			if(linha != null && !linha.isEmpty()) {
+				String[] dados = linha.split("\\;");
+				Pessoa p = new Pessoa(dados[0], dados[1]);
+				pessoas.add(p);
+			}
+		}
+		
+		for (Pessoa pessoa : pessoas) {
+			System.out.println(pessoa);
+		}
+		
+		lerArquivo.close();
+		
+	}
+
+}
